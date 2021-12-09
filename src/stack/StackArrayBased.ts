89ebeec8976b3a732c410d273be2b3b36608300a
Alexander Cheprasov <acheprasov84@gmail.com>
2021-12-09 10:34:44 +0000

    Update StackArrayBased.ts

--- src/stack/StackArrayBased.ts
+++ src/stack/StackArrayBased.ts
@@ -5,12 +5,22 @@ export default class StackArrayBased<T> extends StackInterface<T> {
   protected _items: T[];
 
   constructor(init?: StackArrayBased<T> | T[]) {
+    super();
+
     this._items = [];
     if (init instanceof StackArrayBased) {
       this._items.push(...init._items);
     } else if (Array.isArray(init)) {
       this._items.push(...init);
     }
+  }
+
+  push(item: T): void {
+    this._items.push(item);
+  }
+
+  pop(): T | undefined {
+    return this._items.pop();
   }
 
 }
